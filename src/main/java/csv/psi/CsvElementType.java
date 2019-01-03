51d89406e9adfafd81d3f79157b23f8e1c64e165
Igor <isrpo@yandex.ru>
2019-01-03 18:53:54 +0300

    add CsvElementType

---
+++ src/main/java/csv/psi/CsvElementType.java
@@ -0,0 +1,14 @@
+package csv.psi;
+
+import com.intellij.psi.tree.IElementType;
+import csv.CsvLanguage;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class CsvElementType extends IElementType {
+    public static final IElementType DOCUMENT_START = new CsvElementType("DOCUMENT_START");
+
+    public CsvElementType(@NotNull @NonNls String debugName) {
+        super(debugName, CsvLanguage.INSTANCE);
+    }
+}
