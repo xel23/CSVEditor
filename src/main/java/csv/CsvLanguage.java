43e2d24c13655f3c493936aca2e5b38eb5ebde4f
Igor <isrpo@yandex.ru>
2019-01-03 18:23:04 +0300

    add CsvLanguage

---
+++ src/main/java/csv/CsvLanguage.java
@@ -0,0 +1,17 @@
+package csv;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.PlainTextLanguage;
+
+public final class CsvLanguage extends Language {
+    public static final CsvLanguage INSTANCE = new CsvLanguage();
+
+    private CsvLanguage() {
+        super(PlainTextLanguage.INSTANCE, "csv");
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "CSV";
+    }
+}
