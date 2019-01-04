c306b8292422b4a651983fa512d5719a4cc3bd89
Igor <isrpo@yandex.ru>
2019-01-04 12:52:36 +0300

    add settings.CsvCodeStyleSettings

---
+++ src/main/java/csv/settings/CsvCodeStyleSettings.java
@@ -0,0 +1,65 @@
+package csv.settings;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+//import csv.CsvSeparatorHolder;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.regex.Pattern;
+
+@SuppressWarnings({"checkstyle:membername", "checkstyle:visibilitymodifier"})
+public class CsvCodeStyleSettings extends CustomCodeStyleSettings {
+
+    public boolean SPACE_BEFORE_SEPARATOR = false;
+    public boolean SPACE_AFTER_SEPARATOR = false;
+    public boolean TRIM_LEADING_WHITE_SPACES = false;
+    public boolean TRIM_TRAILING_WHITE_SPACES = false;
+    public boolean TABULARIZE = true;
+    public boolean WHITE_SPACES_OUTSIDE_QUOTES = true;
+    public boolean LEADING_WHITE_SPACES = false;
+    public int SEPARATOR_INDEX = 0;
+    public boolean ENABLE_WIDE_CHARACTER_DETECTION = false;
+    public boolean TREAT_AMBIGUOUS_CHARACTERS_AS_WIDE = false;
+
+    public static final String DEFAULT_SEPARATOR = ",";
+    public static final String TAB_SEPARATOR = "\t";
+    public static final String[] SUPPORTED_SEPARATORS = new String[]{",", ";", "|", TAB_SEPARATOR};
+    public static final String[] SUPPORTED_SEPARATORS_DISPLAY = new String[]{"Comma (,)", "Semicolon (;)", "Pipe (|)", "Tab (↹)"};
+    public static final Pattern REPLACE_DEFAULT_SEPARATOR_PATTERN = Pattern.compile(CsvCodeStyleSettings.DEFAULT_SEPARATOR);
+
+    public static String getCurrentSeparator(CodeStyleSettings codeStyleSettings) {
+        if (codeStyleSettings != null) {
+            CsvCodeStyleSettings csvCodeStyleSettings = codeStyleSettings.getCustomSettings(CsvCodeStyleSettings.class);
+            if (csvCodeStyleSettings != null) {
+                return csvCodeStyleSettings.getSeparator();
+            }
+        }
+        return DEFAULT_SEPARATOR;
+    }
+
+    public static String getCurrentSeparator(@Nullable Project project) {
+        if (!ApplicationManager.getApplication().isUnitTestMode() && project != null) {
+            return getCurrentSeparator(CodeStyleSettingsManager.getInstance(project).getCurrentSettings());
+        }
+        return DEFAULT_SEPARATOR;
+    }
+
+    public static String getCurrentSeparator(@Nullable Project project, @Nullable Language language) {
+        return  "to do";
+    }
+
+    public CsvCodeStyleSettings(CodeStyleSettings settings) {
+        super("CsvCodeStyleSettings", settings);
+    }
+
+    public String getSeparator() {
+        if (SEPARATOR_INDEX < 0 || SEPARATOR_INDEX >= SUPPORTED_SEPARATORS.length) {
+            SEPARATOR_INDEX = 0;
+        }
+        return SUPPORTED_SEPARATORS[SEPARATOR_INDEX];
+    }
+}
