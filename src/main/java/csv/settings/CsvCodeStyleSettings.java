692c8ee529d7efeee017b4c21427de78ced54072
Igor <isrpo@yandex.ru>
2019-01-04 16:42:10 +0300

    changes in CsvCodeStyleSettings

--- src/main/java/csv/settings/CsvCodeStyleSettings.java
+++ src/main/java/csv/settings/CsvCodeStyleSettings.java
@@ -15,14 +15,8 @@ public class CsvCodeStyleSettings extends CustomCodeStyleSettings {
 
     public boolean SPACE_BEFORE_SEPARATOR = false;
     public boolean SPACE_AFTER_SEPARATOR = false;
-    public boolean TRIM_LEADING_WHITE_SPACES = false;
-    public boolean TRIM_TRAILING_WHITE_SPACES = false;
     public boolean TABULARIZE = true;
-    public boolean WHITE_SPACES_OUTSIDE_QUOTES = true;
-    public boolean LEADING_WHITE_SPACES = false;
     public int SEPARATOR_INDEX = 0;
-    public boolean ENABLE_WIDE_CHARACTER_DETECTION = false;
-    public boolean TREAT_AMBIGUOUS_CHARACTERS_AS_WIDE = false;
 
     public static final String DEFAULT_SEPARATOR = ",";
     public static final String TAB_SEPARATOR = "\t";
