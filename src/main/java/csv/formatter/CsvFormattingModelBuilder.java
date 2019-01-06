55f411f0ac62eeb2f5e39421825393f3f11c6fb8
Igor <isrpo@yandex.ru>
2019-01-06 22:00:21 +0300

    add CsvFormattingModelBuilder

---
+++ src/main/java/csv/formatter/CsvFormattingModelBuilder.java
@@ -0,0 +1,37 @@
+package csv.formatter;
+
+import com.intellij.formatting.FormattingModel;
+import com.intellij.formatting.FormattingModelBuilder;
+import com.intellij.formatting.FormattingModelProvider;
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CsvFormattingModelBuilder implements FormattingModelBuilder {
+    @NotNull
+    @Override
+    public FormattingModel createModel(PsiElement element, CodeStyleSettings settings) {
+        ASTNode root = CsvFormatHelper.getRoot(element.getNode());
+        CsvFormattingInfo formattingInfo = new CsvFormattingInfo(
+                settings,
+                CsvFormatHelper.createSpaceBuilder(settings),
+                CsvFormatHelper.createColumnInfoMap(root, settings)
+        );
+
+        return FormattingModelProvider.createFormattingModelForPsiFile(
+                element.getContainingFile(),
+                new CsvBlock(root, formattingInfo),
+                settings
+        );
+    }
+
+    @Nullable
+    @Override
+    public TextRange getRangeAffectingIndent(PsiFile file, int offset, ASTNode elementAtOffset) {
+        return null;
+    }
+}
