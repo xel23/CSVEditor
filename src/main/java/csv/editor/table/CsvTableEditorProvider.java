06f43cc118dd7ad2b8be0a253ee798947ebf9c79
Igor <isrpo@yandex.ru>
2019-01-08 21:16:14 +0300

    add reload methods for CsvTableEditorProvider

--- src/main/java/csv/editor/table/CsvTableEditorProvider.java
+++ src/main/java/csv/editor/table/CsvTableEditorProvider.java
@@ -1,4 +1,54 @@
 package csv.editor.table;
 
-public class CsvTableEditorProvider {
+import com.intellij.openapi.fileEditor.AsyncFileEditorProvider;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorPolicy;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.SingleRootFileViewProvider;
+import csv.editor.CsvFileEditorProvider;
+import csv.editor.table.swing.CsvTableEditorSwing;
+import org.jetbrains.annotations.NotNull;
+
+public class CsvTableEditorProvider implements AsyncFileEditorProvider, DumbAware {
+
+    public static final String EDITOR_TYPE_ID = "csv-table-editor";
+
+    // reload method
+    @Override
+    public String getEditorTypeId() {
+        return EDITOR_TYPE_ID;
+    }
+
+    // reload method
+    @Override
+    public FileEditorPolicy getPolicy() {
+        return FileEditorPolicy.NONE;
+    }
+
+    // reload method
+    @Override
+    public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
+        return CsvFileEditorProvider.isCsvFile(file) && !SingleRootFileViewProvider.isTooLargeForIntelligence(file);
+    }
+
+    // reload method
+    @NotNull
+    @Override
+    public FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+        return createEditorAsync(project, virtualFile).build();
+    }
+
+    // reload method
+    @NotNull
+    @Override
+    public Builder createEditorAsync(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+        return new Builder() {
+            @Override
+            public FileEditor build() {
+                return new CsvTableEditorSwing(project, virtualFile);
+            }
+        };
+    }
 }
